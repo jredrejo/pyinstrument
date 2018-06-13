004a66ca1c363d1f421a4a6d5f968617e699afb3
José L. Redrejo Rodríguez <jredrejo@debian.org>
2018-06-13 19:14:12 +0200

    Add filtering to capture only requests with some text in the url

--- pyinstrument/middleware.py
+++ pyinstrument/middleware.py
@@ -9,12 +9,15 @@ try:
 except:
 	import object as MiddlewareMixin
 
-
 class ProfilerMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        profile_dir = getattr(settings, 'PYINSTRUMENT_PROFILE_DIR', None)
+        self.filter = getattr(settings, 'PYINSTRUMENT_FILTER', None)
+        if self.filter:
+            if self.filter not  in request.get_full_path():
+                return
+        self.profile_dir = getattr(settings, 'PYINSTRUMENT_PROFILE_DIR', None)
 
-        if getattr(settings, 'PYINSTRUMENT_URL_ARGUMENT', 'profile') in request.GET or profile_dir:
+        if getattr(settings, 'PYINSTRUMENT_URL_ARGUMENT', 'profile') in request.GET or self.profile_dir:
             profiler = Profiler()
             profiler.start()
 
@@ -22,25 +25,25 @@ class ProfilerMiddleware(MiddlewareMixin):
 
 
     def process_response(self, request, response):
-        if hasattr(request, 'profiler'):
+        if self.filter:
+            if self.filter not  in request.get_full_path():
+                return response
+        if hasattr(request, 'profiler') or self.profile_dir:
             try:
                 request.profiler.stop()
 
                 output_html = request.profiler.output_html()
-
-                profile_dir = getattr(settings, 'PYINSTRUMENT_PROFILE_DIR', None)
-
-                if profile_dir:
+                if self.profile_dir:
                     filename = '{total_time:.3f}s {path} {timestamp:.0f}.html'.format(
                         total_time=request.profiler.root_frame().time(),
                         path=request.get_full_path().replace('/', '_'),
                         timestamp=time.time()
                     )
 
-                    file_path = os.path.join(profile_dir, filename)
+                    file_path = os.path.join(self.profile_dir, filename)
 
-                    if not os.path.exists(profile_dir):
-                        os.mkdir(profile_dir)
+                    if not os.path.exists(self.profile_dir):
+                        os.mkdir(self.profile_dir)
 
                     with open(file_path, 'w') as f:
                         f.write(output_html)
